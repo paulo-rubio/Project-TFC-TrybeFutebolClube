1afec728be832dd0e837796a1dce3a1d3be6d347
Rubio <paulocezarrubio@gmail.com>
2022-11-22 19:52:04 -0300

    n entendi nada

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,23 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare userName: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+User.init({
+  id: { type: DataTypes.INTEGER, primaryKey: true },
+  userName: DataTypes.STRING,
+  role: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+export default User;
